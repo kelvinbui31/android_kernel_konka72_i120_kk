39d6881d93a58a71a6e364301892e74c11e046f1
kelvinbui31 <kelvinbui31@gmail.com>
2014-11-21 13:03:57 +0700

    fix touch follow the first config

--- mediatek/custom/spark/kernel/touchpanel/msg2133/tpd_custom_msg2133.h
+++ mediatek/custom/spark/kernel/touchpanel/msg2133/tpd_custom_msg2133.h
@@ -2,16 +2,16 @@
 #define TOUCHPANEL_H  
   
 #define MS_TS_MSG21XX_X_MAX                480  
-#define MS_TS_MSG21XX_Y_MAX                854  
+#define MS_TS_MSG21XX_Y_MAX                800  
   
 #define TPD_HAVE_BUTTON  
 #define MAX_TOUCH_FINGER	2
 #define REPORT_PACKET_LENGTH	8
   
   
-#define TPD_BUTTON_HEIGHT   854  
+#define TPD_BUTTON_HEIGHT   800  
 #define TPD_KEY_COUNT           3  
-#define TPD_KEYS                {KEY_BACK, KEY_HOME, KEY_MENU}  
-#define TPD_KEYS_DIM            {{40,880,80,50},{200,880,80,50},{360,880,80,50}}  
+#define TPD_KEYS                {KEY_MENU, KEY_HOME, KEY_BACK}  
+#define TPD_KEYS_DIM            {{60,850,80,50},{180,850,80,50},{300,850,80,50}}  
   
 #endif
